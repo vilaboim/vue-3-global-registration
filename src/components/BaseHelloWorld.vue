da5baba11b98ae39f7ef3151e334aaec628ecf3d
Lucas Vilaboim <lucas@vilaboim.com>
2021-05-07 17:36:17 -0300

    feat(global): register base components globally

--- src/components/BaseHelloWorld.vue
+++ src/components/BaseHelloWorld.vue
@@ -31,7 +31,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'BaseHelloWorld',
   props: {
     msg: String
   }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import upperFirst from 'lodash.upperfirst'
+import camelCase from 'lodash.camelcase'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const requireComponent = require.context(
+  // The relative path of the components folder
+  './components',
+  // Whether or not to look in subfolders
+  true,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName)
+
+  // Get PascalCase name of component
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  // Register component globally
+  app.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  )
+})
+
+app.mount('#app')
